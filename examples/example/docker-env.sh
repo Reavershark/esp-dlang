87826be355ad9ca38f25b699c6929ff01f04d9a1
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-04-07 13:06:49 +0200

    Work around phobos ulimit bug #23684

--- examples/example/docker-env.sh
+++ examples/example/docker-env.sh
@@ -7,6 +7,7 @@ docker run \
     bash -c " \
         echo Sourcing /opt/esp-idf/export.sh && \
         source /opt/esp-idf/export.sh >/dev/null && \
+        ulimit -n 4096 && \
         \$SHELL \
     "
 sudo chown -R $UID .
