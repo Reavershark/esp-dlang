22bb074def972510f287e0741dc4838ce4240624
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-04-07 16:02:37 +0200

    Release v2.0.2: Updated dflags.py

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-depot build . --push -t jmeeuws/esp-dlang:v2.0.1_ldc-v1.37.0_llvm-esp-16.0.4-20231113_esp-idf-v5.2.1
-depot build . --push -t jmeeuws/esp-dlang:v2.0.1
+depot build . --push -t jmeeuws/esp-dlang:v2.0.2_ldc-v1.37.0_llvm-esp-16.0.4-20231113_esp-idf-v5.2.1
+depot build . --push -t jmeeuws/esp-dlang:v2.0.2
 depot build . --push -t jmeeuws/esp-dlang:v2.0
 depot build . --push -t jmeeuws/esp-dlang:v2
 depot build . --push -t jmeeuws/esp-dlang:latest
