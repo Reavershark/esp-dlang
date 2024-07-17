333a07672ff4557cb0cab91cc784dd7842fef536
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-07-17 19:35:52 +0200

    Attempt #3 to fix ci

--- examples/hello_world/idf-project/root.cmake
+++ examples/hello_world/idf-project/root.cmake
@@ -1,4 +1,10 @@
 cmake_minimum_required(VERSION 3.16)
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
 project(hello_world)
-target_link_libraries("${PROJECT_NAME}.elf" "${PROJECT_DIR}/dcode.a")
+
+include($ENV{IDF_PATH}/tools/cmake/version.cmake)
+if (${IDF_VERSION_MAJOR} GREATER_EQUAL 5)
+    target_link_libraries("${PROJECT_NAME}.elf" PRIVATE "${PROJECT_DIR}/dcode.a")
+else()
+    target_link_libraries("${PROJECT_NAME}.elf" "${PROJECT_DIR}/dcode.a")
+endif()
